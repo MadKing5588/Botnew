c13400197353f5c281a7b33ef20364850ca79347
MadKing5588 <106253914+MadKing5588@users.noreply.github.com>
2022-08-20 09:37:15 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['Mád.']
-global.OwnerNumber = ['20103886848']
-global.ownertag = ['Mád.']
+global.Owner = ['+201013886848']
+global.OwnerNumber = ['+20103886848']
+global.ownertag = ['+201013886848']
 global.BotName = "Mád."
 global.packname = "Mád. bot"
 global.author = "By: Mád."
